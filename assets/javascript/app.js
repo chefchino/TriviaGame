d74d47bc5c5b830cc915dc5202980fea27bdfef6
Ciprien Bossut <chefchino@live.com>
2019-10-06 11:32:18 -0400

    questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,22 +18,57 @@ var foodQuestions = [{
 {
     question: "Which city of the world has the most Michellin Stars?",
     answers:["Paris, France", "New York City, New York", "Los Angeles, California", "Tokyo, Japan"],
-    correctAnswer: "Tokyo, Japan"
+    correctAnswer: "Tokyo, Japan",
 },
 {
     question: "Which of the following is a vegetable NOT created by man?",
-    answers:["Brocolli", "Cauliflour"]
+    answers:["Brocolli", "Cauliflour", "Brocollini", "Brussel Sprouts"],
+    correctAnswer: "Brocollini",
+},
+{
+    question: "Where does Black Pepper originate?",
+    answers: ["India", "Italy", "Egypt", "Lebannon"],
+    correctAnswer: "India",
+},
+{
+    question: "Where are the best tomatoes in the world grown?",
+    answers: ["New Jersey, USA", "Parma, Italy", "Bacelona, Spain", "Lima, Peru"],
+    correctAnswer: "New Jersey, USA",
+},
+{
+    question: "Who produces the best Olive Oil?",
+    answers: ["Madrid, Spain", "Sicily, Italy", "Milan, Italy", "Bordeaux, France"],
+    correctAnswer: "Sicily, Italy",
+},
+{
+    question: "Who invented Caesar Dressing and where?",
+    answers: ["A. Escoiffier, France", "Caesar, Mexico", "Emril, Portgual", "Keller, New York City"],
+    correctAnswer: "Caesar, Mexico",
+},
+{
+    question: "What is MirePoix?",
+    answers: ["Soup", "Onions, Carrots, Celery", "Weights used to measure flour", "French Canning Technique"],
+    correctAnswer: "Onions, Carrots, Celery",
+},
+{
+    question: "What does 'Mise en Place' mean?",
+    answers: ["Put in Place", "Prep is Life", "Precise execution", "Chef's Life"],
+    correctAnswer: "Put in Place",
 }]
 // build a timer to countdown time left to answer question
 function startGame(){
+   console.log("food", foodQuestions[0].question);
+   console.log("answer", foodQuestions[0].answers);
+   console.log("correct",foodQuestions[0].correctAnswer);
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
+  
 }
 function decrement(){
     timer--;
     $(".timer").html("<h3>" + timer + "</h3>");
 
-    if(timer === 0){
+    if(timer === 0 ){
         stop();
     }
 }
@@ -42,7 +77,7 @@ function stop() {
 }
 startGame();
 // create questions to be asked
-// give multiple choice and/or true false answers
+// give multiple choice answers
 // if correct answer is selected count +1 for correct answers, display giphy that says correct, timeout to next question
 // if incorrect answer is selected count +1 for incorrect answers, display "wrong", display correct answer, timeout to next question
 // when timer reaches 0, display correct answer, count +1 to incorrect answer, display correct answer, timeout to next question
