b52ebf7462cb9f92549fa88959eed91d955916e1
Ciprien Bossut <chefchino@live.com>
2019-10-10 22:02:32 -0400

    functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,8 @@ var foodQuestions = [{
     answer2: "Marseille, France",
     answer3: "Buenos Aires, Agrentina",
     answer4: "New Jersey, USA",
-    correctAnswer: 'd'
+    correctAnswer: 'd',
+    imgFood:  "assets/images/spaghetti-and-meatballs_hero.jpg",
 },
 {
     question: "Who was dubbed Chef of the Century in the last 10 years?",
@@ -18,7 +19,8 @@ var foodQuestions = [{
     answer2: "Rachel Ray",
     answer3: "Anthony Bourdain",
     answer4: "Andrew Zimmerman",
-    correctAnswer: 'a'
+    correctAnswer: 'a',
+    imgFood: "assets/images/17-paul-bocuse-tattoo.w1200.h630.jpg",
 },
 {
     question: "Which city of the world has the most Michellin Stars?",
@@ -26,7 +28,8 @@ var foodQuestions = [{
     answer2: "New York City, New York",
     answer3: "Los Angeles, California",
     answer4: "Tokyo, Japan",
-    correctAnswer: 'd'
+    correctAnswer: 'd',
+    imgFood: "assets/images/Think-Japan-Tokyo-521830667-SeanPavonePhoto-copy.jpg"
 },
 {
     question: "Which of the following is a vegetable NOT created by man?",
@@ -34,7 +37,8 @@ var foodQuestions = [{
     answer2: "Cauliflour",
     answer3: "Broccolini",
     answer4: "Brussel Sprouts",
-    correctAnswer: 'c'
+    correctAnswer: 'c',
+    imgFood: "assets/images/Broccolini.jpg"
 },
 {
     question: "Where does Black Pepper originate?",
@@ -42,7 +46,8 @@ var foodQuestions = [{
     answer2: "Italy",
     answer3: "Egypt",
     answer4: "Lebannon",
-    correctAnswer: 'a'
+    correctAnswer: 'a',
+    imgFood: "assets/images/Malabar-Black-Pepper.jpg"
 },
 {
     question: "Where are the best tomatoes in the world grown?",
@@ -50,7 +55,8 @@ var foodQuestions = [{
     answer2: "Parma, Italy",
     answer3: "Bacelona, Spain",
     answer4: "Lima, Peru",
-    correctAnswer: 'a'
+    correctAnswer: 'a',
+    imgFood: "assets/images/jersey_tomato.jpg"
 },
 {
     question: "Who produces the best Olive Oil?",
@@ -58,7 +64,8 @@ var foodQuestions = [{
     answer2: "Sicily, Italy",
     answer3: "Milan, Italy",
     answer4: "Bordeaux, France",
-    correctAnswer: 'b'
+    correctAnswer: 'b',
+    imgFood: "assets/images/olive_tree.jpg"
 },
 {
     question: "Who invented Caesar Dressing and where?",
@@ -66,7 +73,8 @@ var foodQuestions = [{
     answer2: "Caesar, Mexico",
     answer3: "Emril, Portgual",
     answer4: "Keller, New York City",
-    correctAnswer: 'b'
+    correctAnswer: 'b',
+    imgFood: "assets/images/mexican-caesar-feature.jpg"
 },
 {
     question: "What is MirePoix?",
@@ -74,7 +82,8 @@ var foodQuestions = [{
     answer2: "Onions, Carrots, Celery",
     answer3: "Weights used to measure flour",
     answer4: "French Canning Technique",
-    correctAnswer: 'b'
+    correctAnswer: 'b',
+    imgFood: "assets/images/mirepoix.jpg"
 },
 {
     question: "What does 'Mise en Place' mean?",
@@ -83,15 +92,25 @@ var foodQuestions = [{
     answer3: "Precise execution",
     answer4: "Chef's Life",
     correctAnswer: 'a',
+    imgFood: "assets/images/mise.jpg"    
 }]
 var lastQuestion = foodQuestions.length - 1;
 var runningQuestion = 0;
 var gameOver = $(".gameOver")
 var jumbotron = $(".jumbotron")
-
+var answers = $(".answers")
+var timeOut;
+document
 function nextQuestion() {
+    clearInterval(intervalId);
+    timer = 5;
+    intervalId = setInterval(decrement, 1000);
+    
+    // clearTimeout(timeOut);
+    $(".answers").empty();
+    $(".answers").append($("<button class='choice' id='a' onclick= checkAnswer('a')></button>"),$("<button class='choice' id='b' onclick= checkAnswer('b')></button>"),$("<button class='choice' id = 'c' onclick= checkAnswer('c')></button>"),$("<button class= 'choice' id= 'd' onclick= checkAnswer('d')></button>"))
     var food = foodQuestions[runningQuestion];
-    timer = 10;
+    
     $(".question").html(food.question);
     $("#a").html(food.answer1);
     $("#b").html(food.answer2);
@@ -103,37 +122,41 @@ function nextQuestion() {
 // $("#reset").on('click', function(){//listener for static
 //     console.log("reset");
 // })
-$(document).on('click', "#reset", function () {//listener for dynamically generated html
-    
-    runningQuesiton = 0;
-    correct = 0;
-    incorrect = 0;
-    nextQuestion();
-    jumbotron.show();
-    
 
-    console.log("reset");
-})
 // nextQuestion();
 function checkAnswer(userChoice) {
     if (userChoice == foodQuestions[runningQuestion].correctAnswer) {
         correct++;
+        $(".answers").empty();
+        var image= $("<img>").attr("src",foodQuestions[runningQuestion].imgFood);
+        $(".answers").append("<h1 id='WINNER'>YOU GOT IT!!!</h1>")
+        $(".answers").append(image);
+        
         console.log("1", correct);
     } else {
         incorrect++;
+        $(".answers").empty();
+        var image= $("<img>").attr("src",foodQuestions[runningQuestion].imgFood);
+        $(".answers").append("<h1 id='LOSER'>'THAT IS A MISS!!!'</h1>")
+        $(".answers").append(image);
         console.log("2", incorrect);
+
     }
-    if (runningQuestion < lastQuestion) {
-        runningQuestion++;
-        nextQuestion();
-    } else {
-        clearInterval(intervalId);
-        // alert("DONE","Correct Answers: ", correct, "Incorrect Answers: ", incorrect);
-        // alert(`DONE Correct Answers: ${correct} |Incorrect Answers: ${incorrect}`);
-        var feedback = $('<div>').html(`<h1>DONE</h1> <p>Correct Answers: ${correct}</p> <p>Incorrect Answers: ${incorrect}</p><button id='reset'>RESET,</button>`);
-        gameOver.append(feedback);
-        jumbotron.hide();
-    }
+     timeOut = setTimeout(function() {
+        console.log("here");
+        if (runningQuestion < lastQuestion) {
+            runningQuestion++;
+            nextQuestion();
+            console.log("question");
+        } else {
+            clearInterval(intervalId);
+            var feedback = $('<div>').html(`"<h1>DONE</h1> <p>Correct Answers: ${correct}</p> <p>Incorrect Answers: ${incorrect}</p>`);
+            gameOver.append(feedback);
+            jumbotron.hide();
+        }
+    }, 3000);
+
+    
 
 
     console.log({ correct, incorrect });
@@ -144,8 +167,25 @@ function decrement() {
 
     if (timer === 0) {
         incorrect++;
-        runningQuestion++;
-        nextQuestion();
+        clearInterval(intervalId);
+        $(".answers").empty();
+        var image= $("<img>").attr("src",foodQuestions[runningQuestion].imgFood);
+        $(".answers").append("<h1 id='TIME'>'TIME IS UP!!!'</h1>")
+        $(".answers").append(image);
+        console.log("outoftime");
+        timeOut = setTimeout(function() {
+            console.log("here");
+            if (runningQuestion < lastQuestion) {
+                runningQuestion++;
+                nextQuestion();
+                console.log("question");
+            } else {
+                clearInterval(intervalId);
+                var feedback = $('<div>').html(`"<h1>DONE</h1> <p>Correct Answers: ${correct}</p> <p>Incorrect Answers: ${incorrect}</p>`);
+                gameOver.append(feedback);
+                jumbotron.hide();
+            }
+        }, 3000);
     }
 
 }
